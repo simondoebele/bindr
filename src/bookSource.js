92030c143d1f9e5c921ad9933be65fc14d654b80
Elvdzi <dzidic@kth.se>
2022-04-13 11:42:59 +0200

    bug fixed

--- src/bookSource.js
+++ src/bookSource.js
@@ -41,33 +41,6 @@ function getDishDetails(id) {
   }).then(treatHTTPResponseACB);
 }
 
-//ISBN version
-/*
-function getBookDetails(isbn) {
-  console.log("fetching: ", isbn);
-  const base_url = "https://openlibrary.org/api/books?bibkeys=ISBN:";
-  return (
-    fetch(base_url + isbn + "&jscmd=data&format=json")
-      // change to isbn + base_url later, or not
-      .then((res) => res.json())
-      .catch((err) => console.log(err))
-  );
-}*/
-
-//https://openlibrary.org/api/books?bibkeys=ISBN:" +
-// "9780385533225" +
-// "&jscmd=data&format=json"
-
-function getDishDetails(id) {
-  return fetch(BASE_URL + "recipes/" + id + "/information", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      "X-Mashape-Key": API_KEY,
-    },
-  }).then(treatHTTPResponseACB);
-}
-
 function searchDishes(params) {
   return fetch(BASE_URL + "recipes/search?" + new URLSearchParams(params), {
     method: "GET",
