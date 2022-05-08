09ef51394be1052eab873f6a39bf58d0bbabb188
Elvdzi <dzidic@kth.se>
2022-05-08 22:11:19 +0200

    new spinning wheel

--- src/views/waitUndef.js
+++ src/views/waitUndef.js
@@ -1,18 +1,11 @@
-function WaitUndef(obj){
-
-
-    if(typeof(obj) == "undefined"){
-        return(
-            <div class = "details">
-                <img src="https://miro.medium.com/max/2400/1*CsJ05WEGfunYMLGfsT2sXA.gif" class="waiting" />
-            </div>
-        )
-            
-
-    } else return
-
-
-
+function WaitUndef(obj) {
+  if (typeof obj == "undefined") {
+    return (
+      <div class="details">
+        <img src="https://i.stack.imgur.com/kOnzy.gif" class="waiting" />
+      </div>
+    );
+  } else return;
 }
 
 export default WaitUndef;
