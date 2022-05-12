84cdab81898304dec512690cedf82fc9cbfe965e
Simon Döbele <simon-doebele@gmx.de>
2022-05-12 09:59:36 +0200

    navbar still moves

--- src/views/bookDetailsView.js
+++ src/views/bookDetailsView.js
@@ -30,7 +30,7 @@ function BookDetailsView(props) {
           </div>
         </div>
       </div>
-      <div class="topnavDetails">
+      <div class="topnavDetails" style="margin-top:55px">
         <div
           class="iconcontain"
           onClick={function () {
